0fb311a88974f22ba9a2094046805c4a285781e3
Omar Flores <omar.flores@3dves.com>
2019-02-07 17:52:37 -0500

    exercise

--- platziverse-db/tests/metric-tests.js
+++ platziverse-db/tests/metric-tests.js
@@ -1,17 +1,42 @@
 'use strict'
 
 const test = require('ava')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 
 let config = {
   loggin: function () {}
 }
+
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentStub = null
 let db = null
+let sandbox = null
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.restore()
 })
 
 test('Metric', t => {
   t.truthy(db.Metric, 'Metric service should exist')
+})
+
+test.serial('Setup', t => {
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
 })
